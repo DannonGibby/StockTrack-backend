1d15d5e9c6358cfb06d6e274fac00b2078fba349
Dannon <dgibby2@wgu.edu>
2025-10-21 23:55:35 -0600

    Initial commit -StockTrack backend

---
+++ src/main/java/com/stocktrack/strocktrack_backend/StrocktrackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.stocktrack.strocktrack_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrocktrackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrocktrackBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stocktrack/strocktrack_backend/StrocktrackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stocktrack.strocktrack_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrocktrackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
